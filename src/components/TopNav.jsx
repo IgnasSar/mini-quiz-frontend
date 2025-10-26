e7f22af1bcadcf7150d92a956e56a49886c7c5a4
Ignas <ignassarapinas@gmail.com>
2025-10-26 15:00:07 +0200

    added navigation bar

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function TopNav() {
+  return (
+    <nav className="topnav">
+      <Link to="/quiz" className="logo">
+        Mini Quiz
+      </Link>
+
+      <div className="nav-actions">
+        <Link to="/quiz" className="btn">
+          Start Quiz
+        </Link>
+        <Link to="/add" className="btn-primary">
+          + Add Question
+        </Link>
+      </div>
+    </nav>
+  );
+}
