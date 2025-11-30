b0716eeb86b7905feacfe1f96b4132419e31a367
Ignas <ignassarapinas@gmail.com>
2025-11-30 20:06:52 +0200

    configuration, dependency setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "unsafe-none"
+    },
   },
 });
